70bc102f120cec441cc7ff191bf6331fa98b6f96
Thea Calaquian <tc.11927@Tristans-MacBook-Pro.local>
2024-11-19 23:40:22 -0800

    secondchallenge

--- class11_exercise.js
+++ class11_exercise.js
@@ -17,6 +17,50 @@ CHALLENGE 2, use the settings to allow the adding the go through or not
 
 
 
+/* PLAN:
+do users registry first, then check registery, then banned list, and check banned list, then the challenges
+
+REGISTRY, CHECK REGISTRY, BANNED LIST, CHECK BANNED:
+Reference loops.js!!! Replace the words. Most of what we taught is in there. Do it in order or how it's laid out
+
+CHALLENGE 1:
+let checkBan=false, use this:
+function ReadNames(){
+    for(let i=0; i<names.length; i++){
+        console.log(`The names are ${names[i]}`);
+    }
+    StartApp();
+}
+We learned in class, but adjust it!!
+
+CHALLENGE 2:
+Okay first edit your prexisting code before adding anything new. Try testing on AddUserToRegistry first before implementing the code everywhere.
+
+Edit these lines:
+
+if(settings.addRegistry === true){
+} else {
+    console.log("Nah bro")
+}
+
+As needed for your code. 
+
+Edit these lines:
+ else if(_command === "Allow Add"){
+    settings.addRegistry = !settings.addRegistry;
+    StartApp();
+  }
+
+As needed for your code.
+
+Check and retry until it works.
+
+GITHUB:
+Push to Github every challenge completed.
+*/
+
+
+
 let users = [];
 let banned = [];
 let settings = {
@@ -27,57 +71,82 @@ let settings = {
 }
 
 function AddUserToRegistry() {
-  
-  // Initialize the boolean flag
-  let checkBan = false;
-  readline.question("What is the username?", _user=>{
-  // Check if the user exists in the ban list
-  if (banned.includes(_user)) {
-      checkBan = true; // Set flag to true if the user is banned
-  }
-  // Add user only if they are not banned
-  if (!checkBan) {
-      users.push(_user);
-      console.log(`${_user} has been added.`);
-  } else {
-      console.log(`${_user} is banned and cannot be added.`);
-  }
-  StartApp();
-})
+  if(settings.addRegistry === true){
+
+    let checkBan = false;
+    readline.question("What is the username?", _user=>{
+    if (banned.includes(_user)) {
+        checkBan = true; 
+    } if (!checkBan) {
+        users.push(_user);
+        console.log(`${_user} has been added.`);
+    } else {
+        console.log(`${_user} is banned and cannot be added.`);
+    }
+    StartApp();
+  })
+
+} else {
+    console.log("Nah bro")
+}
 }
 
 
 function CheckRegistry() {
-  for(let i=0; i<users.length; i++){
+  if(settings.checkRegistry === true){
+  if (!settings.checkRegistry) {
+    console.log("Checking registry is disabled.");
+    return StartApp();
+  } for(let i=0; i<users.length; i++){
     console.log(`The names are ${users[i]}`);
 }
 StartApp();
+} else {
+  console.log("Nah bro")
+}
 }
 
 function BanUser(){
+  if(settings.banPerson === true){
+  if (!settings.banPerson) {
+    console.log("Banning users is disabled.");
+    return StartApp();
+  }
+
   //use readline to prompt for the name of the user to be banned
   readline.question("Who are you banning", _banUsers=>{
     banned.push(_banUsers);
     console.log(banned);
     StartApp();
    })
+  } else {
+    console.log("Nah bro")
+  }
 }
 
 function CheckBanned(){
-  for(let i=0; i< banned.length; i++){
+  if(settings.checkBans === true){
+  if (!settings.checkBans) {
+    console.log("Checking bans is disabled.");
+    return StartApp();
+  } for(let i=0; i< banned.length; i++){
     console.log(`The names are ${banned[i]}`);
 }
 
+
 StartApp();
+} else {
+  console.log("Nah bro")
+}
 }
 
 function StartApp() {
   readline.question("What would you like to do? ", (_command) => {
     
     //add other commands here to add
-    if(_command === "quit"){
+    if(_command === "Quit"){
       readline.close();
-  } else if(_command === "add"){
+  } else if(_command === "Add"){
     AddUserToRegistry();
   }else if(_command ==="Ban"){
       BanUser();
@@ -85,7 +154,19 @@ function StartApp() {
     CheckRegistry();
   } else if(_command === "Check Banned"){
     CheckBanned();
-  }
+  }  else if(_command === "Allow Add"){
+    settings.addRegistry = !settings.addRegistry;
+    StartApp();
+  } else if(_command === "Allow Ban"){
+    settings.banPerson = !settings.banPerson;
+    StartApp();
+  } else if(_command === "Allow Check Add"){
+    settings.checkRegistry = !settings.checkRegistry;
+    StartApp();
+  } else if(_command === "Allow Check Banned"){
+  settings.checkBans = !settings.checkBans;
+  StartApp();
+}
   
     if (_command !== "quit") {
       StartApp();
@@ -94,5 +175,7 @@ function StartApp() {
     }
   });
 }
+
+
 
 StartApp();
